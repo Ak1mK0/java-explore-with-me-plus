6d832d9b9f6e0ab92a4bd97878528d055c313509
Anatoly Yakovlev <ihonhola@yandex.ru>
2026-02-26 23:23:12 +0300

    feat: добавлены dto

---
+++ stat/stat-dto/src/main/java/EndpointHitDto.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // не включать null-поля в JSON
+public class EndpointHitDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Integer id;
+
+    @NotBlank(message = "Идентификатор сервиса не должен быть пустым")
+    private String app;
+
+    @NotBlank(message = "URI не должен быть пустым")
+    private String uri;
+
+    @NotBlank(message = "IP не должен быть пустым")
+    private String ip;
+
+    @NotNull(message = "Дата и время не должны быть пустыми")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
