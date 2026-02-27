bc10923ae9dc228f6ea5598c5707847a4b7c4da9
Anatoly Yakovlev <ihonhola@yandex.ru>
2026-02-27 16:53:59 +0300

    fix: упрощен pom.xml (stat-dto), добавлена валидация IP адреса в EndpointHitDto

--- stat/stat-dto/src/main/java/EndpointHitDto.java
+++ stat/stat-dto/src/main/java/EndpointHitDto.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,6 +27,8 @@ public class EndpointHitDto {
     private String uri;
 
     @NotBlank(message = "IP не должен быть пустым")
+    @Pattern(regexp = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+            message = "IP адрес должен быть в правильном IPv4 формате")
     private String ip;
 
     @NotNull(message = "Дата и время не должны быть пустыми")
