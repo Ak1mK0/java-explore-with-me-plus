6cfadb1d7022399429bc6050390b500ca2463ad8
forsidenis <forsidenis@yandex.ru>
2026-02-27 21:22:36 +0300

    поправил dto и StatsClient на restclient

--- stat/stat-client/src/main/java/ru/practicum/stat/client/StatsClient.java
+++ stat/stat-client/src/main/java/ru/practicum/stat/client/StatsClient.java
@@ -1,59 +1,62 @@
 package ru.practicum.stat.client;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.stat.dto.EndpointHitDto;
 import ru.practicum.stat.dto.ViewStatsDto;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class StatsClient {
-    private final RestTemplate rest;
+    private final RestClient restClient;
     private final String serverUrl;
 
-    // Конструктор с параметрами из application.properties основного сервиса
-    public StatsClient(@Value("${client.url}") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${client.url}") String serverUrl) {
         this.serverUrl = serverUrl;
-        this.rest = builder.build();
+
+        // Настройка клиента с таймаутами
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withReadTimeout(Duration.ofSeconds(10));
+
+        this.restClient = RestClient.builder()
+                .baseUrl(serverUrl)
+                .requestFactory(ClientHttpRequestFactories.get(settings))
+                .build();
     }
 
-    /**
-     * Сохраняет информацию о запросе к эндпоинту.
-     */
     public void hit(EndpointHitDto hit) {
-        rest.postForEntity(serverUrl + "/hit", hit, Void.class);
+        restClient.post()
+                .uri("/hit")
+                .body(hit)
+                .retrieve()
+                .toBodilessEntity(); // 201 Created без тела
     }
 
-    /**
-     * Получает статистику по посещениям за указанный период.
-     *
-     * @param start  начало диапазона
-     * @param end    конец диапазона
-     * @param uris   список uri (может быть null или пустым)
-     * @param unique учитывать только уникальные ip
-     * @return список ViewStatsDto
-     */
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end,
                                        List<String> uris, Boolean unique) {
-        String url = UriComponentsBuilder.fromHttpUrl(serverUrl + "/stats")
+        String uri = UriComponentsBuilder.fromPath("/stats")
                 .queryParam("start", start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 .queryParam("end", end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                .queryParam("uris", uris)   // автоматически преобразуется в несколько параметров uris=...
+                .queryParam("uris", uris)
                 .queryParam("unique", unique)
-                .encode()
+                .build()
                 .toUriString();
 
-        ResponseEntity<ViewStatsDto[]> response = rest.getForEntity(url, ViewStatsDto[].class);
-        return Arrays.asList(response.getBody());
+        ViewStatsDto[] response = restClient.get()
+                .uri(uri)
+                .retrieve()
+                .body(ViewStatsDto[].class);
+
+        return Arrays.asList(response);
     }
 }
--- stat/stat-dto/src/main/java/ru/practicum/stat/dto/StatsRequestDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/stat/dto/StatsRequestDto.java
@@ -1,12 +1,12 @@
 package ru.practicum.stat.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
